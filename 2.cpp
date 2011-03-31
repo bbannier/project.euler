bdb3b4349a12be8257bccf50212a875324af3ec4
Benjamin Bannier <bbannier@gmail.com>
2011-03-31 18:48:41 -0400

    Solution for problem 2

--- 2.cpp
+++ 2.cpp
@@ -7,6 +7,11 @@ struct fib {
 };
 
 template <>
+struct fib<0> {
+  static const size_t value = 0;
+};
+
+template <>
 struct fib<1> {
   static const size_t value = 1;
 };
@@ -16,7 +21,40 @@ struct fib<2> {
   static const size_t value = 2;
 };
 
+template <size_t f>
+struct isEven {
+  static const bool value = f%2==0;
+};
+
+template <size_t n>
+struct nextTerm {
+  static const size_t limit = 4000000;
+  static const size_t value =
+    fib<n+1>::value < limit ?
+      n+1 :
+      0;
+};
+
+template <size_t n=1>
+struct sum {
+  static const size_t value =
+    fib<n>::value
+    *
+    isEven<
+      fib<n>::value
+    >::value
+    +
+    sum<
+      nextTerm<n>::value
+    >::value;
+};
+template <>
+struct sum<0> {
+  static const size_t value = 0;
+};
+
 int main() {
+  // checks for fibonacci generator
   BOOST_STATIC_ASSERT(fib<1>::value == 1);
   BOOST_STATIC_ASSERT(fib<2>::value == 2);
   BOOST_STATIC_ASSERT(fib<3>::value == 3);
@@ -27,4 +65,8 @@ int main() {
   BOOST_STATIC_ASSERT(fib<8>::value == 34);
   BOOST_STATIC_ASSERT(fib<9>::value == 55);
   BOOST_STATIC_ASSERT(fib<10>::value == 89);
+
+  BOOST_STATIC_ASSERT(isEven<10>::value);
+
+  std::cout << sum<>::value << std::endl;
 }
